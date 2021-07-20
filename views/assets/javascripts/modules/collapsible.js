585ce4df530b05baf54e0c7c361c6cff8ffec13e
Konstantinos Markopoulos <kos@tyk.io>
2021-07-20 18:20:05 +0100

    Handle scenario when there's no arrow icon. Rename object key

--- views/assets/javascripts/modules/collapsible.js
+++ views/assets/javascripts/modules/collapsible.js
@@ -1,21 +1,21 @@
 export function Collapsible (options) {
-	let clickableElement = [...document.getElementsByClassName(options?.headerSelector || 'collapsible-header')];
+	let clickableElement = [...document.getElementsByClassName(options?.clickableSelector || 'collapsible-header')];
 	let collapsibleContent = [...document.getElementsByClassName(options?.contentSelector || 'collapsible-content')]
 
 	clickableElement.forEach((item,index) => {
   	item.addEventListener('click', () => {
-  		let arrowIcon = clickableElement[index].children[0];
+  		let arrowIcon = clickableElement[index].children[0].classList[0] === 'tyk-icon' ? clickableElement[index].children[0] : null;
 			if(
 					collapsibleContent[index].style.display.length === 0 ||
 					collapsibleContent[index].style.display === 'none'
 				) {
 				collapsibleContent[index].style.display = 'block'
-				arrowIcon.classList.remove('tykon-arrowup');
-				arrowIcon.classList.add('tykon-arrowdown');
+				arrowIcon?.classList.remove('tykon-arrowup');
+				arrowIcon?.classList.add('tykon-arrowdown');
 			} else {
 				collapsibleContent[index].style.display = 'none'
-				arrowIcon.classList.remove('tykon-arrowdown');
-				arrowIcon.classList.add('tykon-arrowup');
+				arrowIcon?.classList.remove('tykon-arrowdown');
+				arrowIcon?.classList.add('tykon-arrowup');
 			}
   	});
 	});

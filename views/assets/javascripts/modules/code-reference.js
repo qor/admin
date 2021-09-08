d51e196157770350d3878ce2fb262f346720a796
cmelgarejo <cmelgarejo.dev@gmail.com>
2021-09-08 19:35:44 -0400

    Removes unneeded file
    Signed-off-by: cmelgarejo <cmelgarejo.dev@gmail.com>

--- views/assets/javascripts/modules/code-reference.js
+++
@@ -1,7 +0,0 @@
-export function CodeReferenceOnChange(inputEl, value) {
-  if (inputEl.value.match(/\.{1,}/)) return;
-  const labelValue =
-    inputEl.parentElement.parentElement.parentElement.lastElementChild.lastElementChild.lastElementChild.innerText;
-  inputEl.parentElement.parentElement.parentElement.lastElementChild.lastElementChild.lastElementChild.innerText =
-    labelValue.replace(/\.blocks\..*?\./g, `.blocks.${value}.`);
-}

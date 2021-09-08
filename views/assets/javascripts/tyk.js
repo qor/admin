99b21e2d415583a75cd12980d59d069b2abea4b7
cmelgarejo <cmelgarejo.dev@gmail.com>
2021-09-07 20:23:59 -0400

    WIP!
    Signed-off-by: cmelgarejo <cmelgarejo.dev@gmail.com>

--- views/assets/javascripts/tyk.js
+++ views/assets/javascripts/tyk.js
@@ -1,13 +1,20 @@
 /* On form cancel */
 function onCancel() {
-	let paths = window.location.pathname.split('/').filter(path => path);
-	let backURL = window.location.origin + '/' + paths[0] + '/' + paths[1];
+  let paths = window.location.pathname.split('/').filter((path) => path);
+  let backURL = window.location.origin + '/' + paths[0] + '/' + paths[1];
 
-	/* If it has opened on a new page, needs to go back with page reload otherwise without */
-	if($('.qor-slideout').hasClass('is-slided')) {
-		window.history.pushState({}, null, backURL);
-	} else {
-		window.location.href = backURL;
-	}
+  /* If it has opened on a new page, needs to go back with page reload otherwise without */
+  if ($('.qor-slideout').hasClass('is-slided')) {
+    window.history.pushState({}, null, backURL);
+  } else {
+    window.location.href = backURL;
+  }
 }
 
+function CodeReferenceOnChange(inputEl, value) {
+  if (inputEl.value.match(/\.{1,}/)) return;
+  const labelValue =
+    inputEl.parentElement.parentElement.parentElement.lastElementChild.lastElementChild.lastElementChild.innerText;
+  inputEl.parentElement.parentElement.parentElement.lastElementChild.lastElementChild.lastElementChild.innerText =
+    labelValue.replace(/\.blocks\..*?\./g, `.blocks.${value}.`);
+}

e6473662ebdcb2e3ab9db55c6c574a807de723b0
cmelgarejo <cmelgarejo.dev@gmail.com>
2021-09-15 16:31:49 -0400

    Code reference placeholder should be empty
    Signed-off-by: cmelgarejo <cmelgarejo.dev@gmail.com>

--- views/assets/javascripts/tyk.js
+++ views/assets/javascripts/tyk.js
@@ -1,34 +1,36 @@
 /* On form cancel */
 function onCancel() {
-    let paths = window.location.pathname.split('/').filter((path) => path);
-    let backURL = window.location.origin + '/' + paths[0] + '/' + paths[1];
+  let paths = window.location.pathname.split('/').filter((path) => path);
+  let backURL = window.location.origin + '/' + paths[0] + '/' + paths[1];
 
-    /* If it has opened on a new page, needs to go back with page reload otherwise without */
-    if ($('.qor-slideout').hasClass('is-slided')) {
-        window.history.pushState({}, null, backURL);
-    } else {
-        window.location.href = backURL;
-    }
+  /* If it has opened on a new page, needs to go back with page reload otherwise without */
+  if ($('.qor-slideout').hasClass('is-slided')) {
+    window.history.pushState({}, null, backURL);
+  } else {
+    window.location.href = backURL;
+  }
 }
 
 function CodeReferenceOnChange(inputId, value, crType) {
-    const codeRefEl = document.getElementById(inputId);
-    let crValue;
-    switch (crType) {
-        case 'content':
-            crValue = `safe .blocks.${value}.Content`;
-            break;
-        case 'image':
-            crValue = `.blocks.${value}.Image.URL`;
-            break;
-        default:
-            crValue = `.blocks.${value}.Content`;
-            break;
-    }
-    codeRefEl.value = `{{ ${crValue} }}`;
+  const codeRefContainer = document.getElementById(`${inputId}_container`);
+  codeRefContainer.style.display = 'flex';
+  const codeRefEl = document.getElementById(inputId);
+  let crValue;
+  switch (crType) {
+    case 'content':
+      crValue = `safe .blocks.${value}.Content`;
+      break;
+    case 'image':
+      crValue = `.blocks.${value}.Image.URL`;
+      break;
+    default:
+      crValue = `.blocks.${value}.Content`;
+      break;
+  }
+  codeRefEl.value = value == '' ? '' : `{{ ${crValue} }}`;
 }
 
 function copyToClipboard(elemId) {
-    const codeRefEl = document.getElementById(elemId);
-    navigator.clipboard.writeText(codeRefEl.value);
+  const codeRefEl = document.getElementById(elemId);
+  navigator.clipboard.writeText(codeRefEl.value);
 }

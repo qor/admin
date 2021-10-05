1beec34551a1ad6b65d83347032a49c75b3b5612
Konstantinos Markopoulos <kos@tyk.io>
2021-10-05 10:13:55 +0100

    Tweak collapsible to be more flexible. Finalise the cards

--- views/assets/javascripts/modules/collapsible.js
+++ views/assets/javascripts/modules/collapsible.js
@@ -6,18 +6,15 @@ export function Collapsible (options) {
   	item.addEventListener('click', () => {
       let contentStyle = collapsibleContent[index].style;
   		let arrowIcon = clickableElement[index].children[0]?.classList[0] === 'tyk-icon' ? clickableElement[index].children[0] : null;
-			if(
-					contentStyle.display.length === 0 ||
-					contentStyle.display === 'none'
-				) {
-				contentStyle.display = 'block';
-				arrowIcon?.classList.remove('tykon-arrowup');
-				arrowIcon?.classList.add('tykon-arrowdown');
-			} else {
-				contentStyle.display = 'none';
-				arrowIcon?.classList.remove('tykon-arrowdown');
-				arrowIcon?.classList.add('tykon-arrowup');
-			}
+        if(contentStyle.display === 'none' || window.getComputedStyle(collapsibleContent[index], null).display === 'none') {
+          contentStyle.display = 'block';
+          arrowIcon?.classList.remove('tykon-arrowup');
+          arrowIcon?.classList.add('tykon-arrowdown');
+        } else {
+          contentStyle.display = 'none';
+          arrowIcon?.classList.remove('tykon-arrowdown');
+          arrowIcon?.classList.add('tykon-arrowup');
+        }
   	});
 	});
 };
